0954161d818729d9d00fca2c065776bbc228c049
Khanh Minh <khanhminh@Mac.lan>
2024-11-07 22:44:32 +0700

    feat: setup unit test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import ReactLogo from './assets/react.svg';
+import Footer from '@components/footer';
+import Header from '@components/header';
+import Home from '@pages/Home';
+
 function App() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold text-gray-900">This is an e-commerce website</h1>
-      <div className="mb-6 mt-8 flex items-center gap-4">
-        <img src="./vite.svg" alt="vite_logo" width={160} height={160} />
-        <p className="text-6xl">+</p>
-        <img src={ReactLogo} alt="react_logo" width={160} height={160} />
-      </div>
-      <h1 className="text-4xl font-bold text-gray-800">Create your new project together.</h1>
+    <main className="min-h-screen">
+      <Header />
+      <Home />
+      <Footer />
     </main>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@styles': resolve(__dirname, './src/styles'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+  },
+});
