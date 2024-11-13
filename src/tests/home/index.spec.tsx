5e90d39f6d795ff5ef5809b10961809588db2a19
Khanh Minh <khanhminh@Mac.lan>
2024-11-13 22:56:51 +0700

    chore: test sonar cloud

--- src/tests/home/index.spec.tsx
+++ src/tests/home/index.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
 describe('Home Page', () => {
-  test('render', () => {
+  test('render Home page', () => {
     const screen = render(<Home />);
     expect(screen.getByText('FIND CLOTHES THAT MATCHES YOUR STYLE')).toBeInTheDocument();
   });
